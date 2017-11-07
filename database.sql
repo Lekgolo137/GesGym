1fdd27e59feef0864455fc33e2c92fd186c0749c
Lekgolo137 <33449941+Lekgolo137@users.noreply.github.com>
2017-11-07 16:41:55 +0100

    Add files via upload

--- database.sql
+++ database.sql
@@ -2,77 +2,82 @@ create database gesgym;
 
 use gesgym;
 
---Heredado del ejemplo original, necesita modificaciones.
+--Completo
 create table users (
 		username varchar(255),
 		passwd varchar(255),
+		tlf int(9),
+		tipo ENUM('cliente','entrenador','administrador'),
+		calle varchar(255),
+		ciudad varchar(255),
+		codPostal varchar(255),
 		
 		primary key (username)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Heredado del ejemplo original, necesita ser eliminado.
-create table posts (
-	id int auto_increment,
-	title varchar(255),
-	content varchar(255),
-	author varchar(255) not null,
-
-	primary key (id),
-	foreign key (author) references users(username)
-) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
-
---Heredado del ejemplo original, necesita modificaciones.
+--Completo
 create table comments (
-	id int auto_increment,	 
+	commentid varchar(255),	 
 	content varchar(255),
-	author varchar(255) not null,
-	post int not null,
+	username varchar(255),
+	sessionid varchar(255),
+	tableid varchar(255),
 
-	primary key (id),
-	foreign key (author) references users(username),
-	foreign key (post) references posts(id) on delete cascade
+	primary key (commentid),
+	foreign key (username) references users(username),
+	foreign key (sessionid) references sessions(sessionid),
+	foreign key (tableid) references tables(tableid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table tables (
 		tableid varchar(255),
+		tabletipo ENUM('person','noPerson'),
+		
 		primary key (tableid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table sessions (
 		sessionid varchar(255),
 		username varchar(255),
 		tableid varchar(255),
-		commentid int auto_increment,
+		fechaInicio datetime,
+		fichaFin datetime,
 		
 		primary key (sessionid),
 		foreign key (username) references users(username),
-		foreign key (tableid) references tables(tableid),
-		foreign key (commentid) references comments(id)
+		foreign key (tableid) references tables(tableid)
+		
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table exercises (
 		exerciseid varchar(255),
+		exe_tipo ENUM('cardio','musculacion','estiramiento'),
 		
 		primary key (exerciseid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table activities (
 		activityid varchar(255),
+		plazas int(10),
+		
 		primary key (activityid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos, usar el mismo atributo para aforo y cantidad y así evitar tener una tabla más.
+--Completo
 create table resources (
 		resourceid varchar(255),
+		resource_tipo ENUM('instalacion','material'),
+		location varchar(255),
+		cant_afor int(10),
 		
 		primary key (resourceid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table act_use_res (
 		activityid varchar(255),
 		resourceid varchar(255),
@@ -81,19 +86,23 @@ create table act_use_res (
 		foreign key (resourceid) references resources(resourceid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table use_participates_act (
 		username varchar(255),
 		activityid varchar(255),
+		fecha datetime,
 		
 		foreign key (username) references users(username),
 		foreign key (activityid) references activities(activityid)
 ) ENGINE=INNODB DEFAULT CHARACTER SET = utf8;
 
---Faltan atributos.
+--Completo
 create table tab_has_exe (
 		tableid varchar(255),
 		exerciseid varchar(255),
+		series int(10),
+		repe int(10),
+		tiempo int(7),
 		
 		foreign key (tableid) references tables(tableid),
 		foreign key (exerciseid) references exercises(exerciseid)
