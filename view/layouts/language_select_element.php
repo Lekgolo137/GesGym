df1bca6cdbf422ea30c8f8fb67a69146701d24a9
Lekgolo137 <33449941+Lekgolo137@users.noreply.github.com>
2017-11-17 19:23:36 +0100

    Delete language_select_element.php

--- view/layouts/language_select_element.php
+++
@@ -1,8 +0,0 @@
-<?php
-// file: view/layouts/language_select_element.php
-?>
-			<div id="languagechooser">
-				<?=i18n("Language")?>:
-				<a href="index.php?controller=language&amp;action=change&amp;lang=es"><img src="img/es.gif" alt="<?=i18n("Spanish")?>"></a>
-				<a href="index.php?controller=language&amp;action=change&amp;lang=en"><img src="img/en.gif" alt="<?=i18n("English")?>"></a>
-			</div>
