5515044e7601fb39d2fa1e0ddf709c79fc9b9b3d
Lekgolo137 <33449941+Lekgolo137@users.noreply.github.com>
2017-11-22 17:13:23 +0100

    Version Preliminar Final 2ª Parte

--- view/tables/edit.php
+++ view/tables/edit.php
@@ -4,23 +4,21 @@ $view = ViewManager::getInstance();
 
 $tables = $view->getVariable("tables");
 $errors = $view->getVariable("errors");
-
+$view->setVariable("title", i18n("GesGym - Modify Table"));
+$view->setVariable("header", i18n("Modify Table"));
 ?>
-<div class="container">
-<h1><?= i18n("Modify table") ?></h1>
+
 <form class="form-signin" action="index.php?controller=tables&amp;action=edit" method="POST">
-	<div class="row">
-		<div class="col-sm-3">
-	<?= i18n("tableid") ?>: <input class="form-control" type="text" name="tableid"
+
+	<?= i18n("ID") ?>: <input disabled class="form-control" type="text" name="tableid"
 	value="<?= isset($_POST["tableid"])?$_POST["tableid"]:$tables->getTableid() ?>">
 	<?= isset($errors["tableid"])?i18n($errors["tableid"]):"" ?><br>
 
-	<?= i18n("tabletipo") ?>: <select class="form-control" name="tabletipo">
-	<option value="person">person</option>
-	<option value="noPerson">noPerson</option>
-
+	<?= i18n("Type") ?>: <select class="form-control" name="tabletipo">
+	<option value="person" <?php if ($tables->getTableTipo() == "person") print "selected"?>><?=i18n("Custom")?></option>
+	<option value="noPerson" <?php if ($tables->getTableTipo() == "noPerson") print "selected"?>><?=i18n("Standard")?></option>
 	<input class="form-control" type="hidden" name="id" value="<?= $tables->getTableid() ?>">
-	<input class="form-control" type="submit" name="submit" value="<?= i18n("Modify post") ?>">
+	<input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="<?= i18n("Save changes") ?>">
 </form>
-</div></div>
-</div>
+<a class="btn btn-lg btn-primary btn-block" href="index.php?controller=tables&amp;action=tablesList"><?=i18n("Cancel")?></a>
+<?=$view->moveToFragment("css")?>		<link rel="stylesheet" type="text/css" href="css/editTableStyle.css">
