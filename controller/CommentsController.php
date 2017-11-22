e44bc9eb157c4df0c0aa48b376dadbb53a043ad7
Lekgolo137 <33449941+Lekgolo137@users.noreply.github.com>
2017-11-22 01:17:37 +0100

    Profile 1/2
    Añadido profile, aún hay que arreglar varios cosas. Commit 1 de 2 (demasiados archivos para un solo commit).

--- controller/CommentsController.php
+++ controller/CommentsController.php
@@ -1,3 +1,132 @@
 <?php
-//file: controller/CommentsController.php
+require_once(__DIR__."/../model/Comment.php");
+require_once(__DIR__."/../model/Comment.php");
+require_once(__DIR__."/../model/CommentMapper.php");
+require_once(__DIR__."/../model/User.php");
 
+require_once(__DIR__."/../core/ViewManager.php");
+require_once(__DIR__."/../controller/BaseController.php");
+
+class CommentsController extends BaseController {
+  //Reference to the tableMapper to interact
+  private $commentMapper;
+
+  public function __construct() {
+    parent::__construct();
+
+    $this->commentMapper = new CommentMapper();
+  }
+
+  /***********************************************************************************/
+  /***********************************************************************************/
+  //Action to list tables
+  public function index() {
+    if (!isset($this->currentUser)) {
+      throw new Exception("Not in session. This action requires login");
+    }
+
+    if (!isset($_REQUEST["id"])) {
+      $comments = $comments = new Comment();
+    }else{
+      // obtain the data from the database
+      $comments = $this->commentMapper->findBySessionId($_REQUEST['id']);
+    }
+
+    // put the array containing Table object to the view
+    $this->view->setVariable("comments", $comments);
+
+    // render the view (/view/tables/index.php)
+    $this->view->render("comments", "index");
+  }
+
+  /*************************************************************************************/
+  /***********************************************************************************/
+  //Action to add a new Table
+  public function add() {
+    if (!isset($this->currentUser)) {
+      throw new Exception("Not in session. This action requires login");
+    }
+
+    $comments = new Comment();
+
+    if (isset($_POST["submit"])) { // reaching via HTTP Table...
+
+      // populate the Table object with data form the form
+      $comments->setCommentid($_POST["commentid"]);
+      $comments->setContent($_POST["content"]);
+      $comments->setUsername($_POST["username"]);
+      $comments->setSessionid($_POST["sessionid"]);
+      $comments->setTableid($_POST["tableid"]);
+
+
+      try {
+
+        // save the Table object into the database
+        $this->commentMapper->save($comments);
+
+        // POST-REDIRECT-GET
+        // Everything OK, we will redirect the user to the list of tables
+        // We want to see a message after redirection, so we establish
+        // a "flash" message (which is simply a Session variable) to be
+        // get in the view after redirection.
+        $this->view->setFlash(sprintf(i18n("Table \"%s\" successfully added."),$comments->getCommentid()));
+
+        $this->view->setVariable("comments", $comments);
+        $this->view->redirect("comments", "add");
+
+      }catch(ValidationException $ex) {
+        // Get the errors array inside the exepction...
+        $errors = $ex->getErrors();
+        // And put it to the view as "errors" variable
+        $this->view->setVariable("errors", $errors);
+      }
+    }
+
+    // Put the Table object visible to the view
+    $this->view->setVariable("comments", $comments);
+
+    // render the view (/view/tables/add.php)
+    $this->view->render("comments", "add");
+
+  }
+
+  /*********************************************************************************/
+  /***********************************************************************************/
+  //Action to delete a table
+  public function delete() {
+    if (!isset($this->currentUser)) {
+      throw new Exception("Not in session. This action requires login");
+    }
+
+    if (!isset($_POST["id"])) {
+      throw new Exception("id is mandatory");
+    }
+
+    // Get the Table object from the database
+    $commentid = $_REQUEST["id"];
+    $comments = $this->commentMapper->findById($commentid);
+
+    // Does the table exist?
+    if ($comments == NULL) {
+      throw new Exception("no such table with id: ".$commentid);
+    }
+
+
+    // Delete the Table object from the database
+    $this->commentMapper->delete($comments);
+
+    // POST-REDIRECT-GET
+    // Everything OK, we will redirect the user to the list of tables
+    // We want to see a message after redirection, so we establish
+    // a "flash" message (which is simply a Session variable) to be
+    // get in the view after redirection.
+    $this->view->setFlash(sprintf(i18n("Table \"%s\" successfully deleted."),$comments ->getCommentid()));
+
+    // perform the redirection. More or less:
+    // header("Location: index.php?controller=tables&action=index")
+    // die();
+    $this->view->setVariable("comments", $comments);
+    $this->view->redirect("comments", "index");
+  }
+
+}
--- controller/SessionsController.php
+++ controller/SessionsController.php
@@ -1,3 +1,124 @@
 <?php
-//file: controller/SessionsController.php
+require_once(__DIR__."/../model/Comment.php");
+require_once(__DIR__."/../model/Session.php");
+require_once(__DIR__."/../model/SessionMapper.php");
+require_once(__DIR__."/../model/User.php");
 
+require_once(__DIR__."/../core/ViewManager.php");
+require_once(__DIR__."/../controller/BaseController.php");
+
+class SessionsController extends BaseController {
+  //Reference to the tableMapper to interact
+  private $sessionMapper;
+
+  public function __construct() {
+    parent::__construct();
+
+    $this->sessionMapper = new SessionMapper();
+  }
+
+  /***********************************************************************************/
+  /***********************************************************************************/
+  //Action to list sessions
+  public function index() {
+    if (!isset($this->currentUser)) {
+      throw new Exception("Not in session. This action requires login");
+    }
+
+    // obtain the data from the database
+    $sessions = $this->sessionMapper->findAll();
+
+    // put the array containing Session object to the view
+    $this->view->setVariable("sessions", $sessions);
+
+    // render the view (/view/session/index.php)
+    $this->view->render("sessions", "index");
+  }
+
+  /*************************************************************************************/
+  /***********************************************************************************/
+  //Action to add a new Table
+  public function add() {
+    if (!isset($this->currentUser)) {
+      throw new Exception("Not in session. This action requires login");
+    }
+
+    $sessions = new Session();
+
+    if (isset($_POST["submit"])) { // reaching via HTTP Table...
+
+      // populate the Table object with data form the form
+      $sessions->setSessionid($_POST["sessionid"]);
+      $sessions->setUsername($_POST["username"]);
+      $sessions->setTableid($_POST["tableid"]);
+      //$sessions->setFechaInicio($fecha);
+      $sessions->setFechaFin(NULL);
+
+      try {
+
+        // save the Table object into the database
+        $this->sessionMapper->save($sessions);
+
+        // POST-REDIRECT-GET
+        // Everything OK, we will redirect the user to the list of tables
+        // We want to see a message after redirection, so we establish
+        // a "flash" message (which is simply a Session variable) to be
+        // get in the view after redirection.
+        $this->view->setFlash(sprintf(i18n("Table \"%s\" successfully added."),$sessions ->getSessionid()));
+
+        $this->view->redirect("sessions", "index");
+
+      }catch(ValidationException $ex) {
+        // Get the errors array inside the exepction...
+        $errors = $ex->getErrors();
+        // And put it to the view as "errors" variable
+        $this->view->setVariable("errors", $errors);
+      }
+    }
+
+    // Put the Table object visible to the view
+    $this->view->setVariable("sessions", $sessions);
+
+    // render the view (/view/tables/add.php)
+    $this->view->render("sessions", "add");
+
+  }
+
+  /*********************************************************************************/
+  /***********************************************************************************/
+  //Action to close a session
+  public function close() {
+    if (!isset($this->currentUser)) {
+      throw new Exception("Not in session. This action requires login");
+    }
+
+    if (!isset($_POST["id"])) {
+      throw new Exception("id is mandatory");
+    }
+
+
+    // Get the Table object from the database
+    $sessionsid = $_REQUEST["id"];
+    $sessions = $this->sessionMapper->findById($sessionsid);
+
+    // Does the table exist?
+    if ($sessions == NULL) {
+      throw new Exception("no such table with id: ".$sessionsid);
+    }
+
+    // Delete the Table object from the database
+    $this->sessionMapper->close($sessions);
+
+    // POST-REDIRECT-GET
+    // Everything OK, we will redirect the user to the list of tables
+    // We want to see a message after redirection, so we establish
+    // a "flash" message (which is simply a Session variable) to be
+    // get in the view after redirection.
+    $this->view->setFlash(sprintf(i18n("Table \"%s\" successfully deleted."),$sessions ->getSessionid()));
+
+    // perform the redirection. More or less:
+    // header("Location: index.php?controller=tables&action=index")
+    // die();
+    $this->view->redirect("sessions", "index");
+  }
+}
